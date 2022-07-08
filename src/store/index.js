6b88fec0ed7a041d1182e5c6540b82e0eafe0db8
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-07-08 12:24:04 -0400

    attaching payloads to actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,25 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = {counter: 0}, action) => { 
+const counterReducer = (state = { counter: 0 }, action) => {
     if (action.type === "increment") {
         return {
             counter: state.counter + 1,
         };
     }
-    if (action.type === "decrement") {
+    if (action.type === "increase") {
         return {
-            counter: state.counter - 1,
-        }
+            counter: state.counter + action.amount,
+        };
     }
-    return state;
-};
+        if (action.type === "decrement") {
+            return {
+                counter: state.counter - 1,
+            }
+        }
 
-const store = createStore(counterReducer);
+        return state;
+    };
 
-export default store;
+    const store = createStore(counterReducer);
+
+    export default store;
